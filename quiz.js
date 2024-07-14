d8966be5071b4845985c4ead5de473ce91abcfcc
FavyWorld <adeosunfavoradewole@gmail.com>
2024-07-14 20:10:24 +0100

    index.html

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function checkAnswer(){
+    let correctAnswer= 4;
+    let selectedOption= document.querySelector('input[name=quiz]: checked');
+   
+    if(selectedOption){
+        let userAnswer=selectedOption.value;
+        let feedbackElement=document.getElementById('feedback');
+        if(userAnswer === correctAnswer){
+            feedbackElement.textContent = 'Correct! Well done';
+        }
+        else{
+            feedbackElement.textContent ="That's incorrect. Try again";
+        }
+    } 
+    else{
+        // if no option is selected
+        document.getElementById('feedback').textContent ='Please select an answer.';
+    }
+};
+
